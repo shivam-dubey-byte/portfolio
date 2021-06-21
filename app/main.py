2c3c82f4936c586487ac183e193b99f55f9cad63
shivam-dubey-byte <74610989+shivam-dubey-byte@users.noreply.github.com>
2021-06-21 22:29:44 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template, session, redirect, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def __index__():
-	return render_template('index.html')
-if __name__ == '__main__':
-	app.run()
+@app.route("/")
+def home():
+    return render_template('index.html')
+if '__name__'=='__main__':
+    app.run(debug=True)
