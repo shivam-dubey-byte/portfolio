7a7eac8ad6bf359041c91517920bdc1d4914d519
shivam-dubey-byte <74610989+shivam-dubey-byte@users.noreply.github.com>
2021-06-21 22:25:02 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,6 +2,6 @@ from flask import Flask, render_template, session, redirect, request
 
 app = Flask(__name__)
 
-app.route(/)
+@app.route('/')
 def __index__():
 	return render_template('index.html')
